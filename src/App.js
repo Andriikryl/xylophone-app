8ba72bd9dc6f44839b5967f55b5da27156108874
Andriikryl <andreyk557@gmail.com>
2023-04-12 00:10:00 +0300

    feat[style / replay / clear -btns]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
+import { useState } from "react";
 import "./App.css";
 import notes from "./assets/notes.js";
 
 const NoteButton = (props) => {
   return (
-    <button onClick={() => new Audio(props.note.file).play()}>
+    <button
+      className="note-button"
+      style={{ height: 200 - 15 * props.index + "px" }}
+      onClick={() => {
+        props.setPlayNotes([...props.playedNotes, props.note]);
+        new Audio(props.note.file).play();
+      }}
+    >
+      <div className="circle" />
       {props.note.name}
+      <div className="circle" />
     </button>
   );
 };
 
 function App() {
+  const [playedNotes, setPlayNotes] = useState([]);
+
+  const replayNotes = () => {
+    playedNotes.map((note, idnex) => {
+      window.setTimeout(() => new Audio(note.file).play(), 500 * idnex);
+    });
+  };
+
+  const resetNotes = () => {
+    setPlayNotes([]);
+  };
+
   return (
     <div className="App">
+      <h1>react xylophone</h1>
       <div className="page">
-        <h1>Create your react xylophone</h1>
         <div className="xylophone">
-          {notes.map((note) => {
-            return <NoteButton note={note}></NoteButton>;
+          {notes.map((note, index) => {
+            return (
+              <NoteButton
+                index={index}
+                playedNotes={playedNotes}
+                setPlayNotes={setPlayNotes}
+                key={note.name}
+                note={note}
+              ></NoteButton>
+            );
           })}
         </div>
-        {/* <button>Replay</button> */}
-        {/* <button>Clear</button> */}
+        {JSON.stringify(playedNotes)}
+        <button onClick={() => replayNotes()}>Replay</button>
+        <button onClick={() => resetNotes()}>Clear</button>
       </div>
     </div>
   );
