e181d70ed67c3ffa5ba7af9f76fc911bc02c8f19
Andriikryl <andreyk557@gmail.com>
2023-04-11 22:22:50 +0300

    feat[btn-component]

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import notes from "./assets/notes.js";
+
+const NoteButton = (props) => {
+  return (
+    <button onClick={() => new Audio(props.note.file).play()}>
+      {props.note.name}
+    </button>
+  );
+};
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="page">
+        <h1>Create your react xylophone</h1>
+        <div className="xylophone">
+          {notes.map((note) => {
+            return <NoteButton note={note}></NoteButton>;
+          })}
+        </div>
+        {/* <button>Replay</button> */}
+        {/* <button>Clear</button> */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
